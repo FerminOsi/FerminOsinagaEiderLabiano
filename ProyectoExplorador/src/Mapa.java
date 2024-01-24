2a1a7026898998a56ff4f7123137eefcb2b9f203
Ferminosinaga <xPlash.business@gmail.com>
2024-01-24 13:13:06 +0100

    Arreglando Mapa los bucles

--- ProyectoExplorador/src/Mapa.java
+++ ProyectoExplorador/src/Mapa.java
@@ -1,33 +1,40 @@
 import java.util.Random;
 public class Mapa {
-    private final int[][] tablero = new int[10][10];
+    private final int[][] tablero;
     private Posicion posTesoro;
     Random r = new Random();
-
     public Mapa() {
+        this.tablero = new int[10][10];
+        int r = -1;
+        for (int x = 0; x < this.tablero.length; x++) {
 
-        for (int x = 0; x < tablero.length; x++) {
-            for (int y = 0; y < tablero[x].length; y++) {
-                if (y != 0 && y != 9){
-                    tablero[x][y] = 0;
-                }
-                else if (y == 9) {
-                    y = tablero[x].length;
-                    int r = (int) (Math.random() * 11 + 1);
-                    tablero[y][r] = 2;
-                } else if (x !=1 && x != 9 ){
-                    int r = (int) (Math.random() * 101 + 1);
-                    tablero[x][r] = 2;
+            for (int y = 0; y < this.tablero[x].length; y++) {
+
+                if (x == 0) {
+                    this.tablero[x][y] = 0;
+
+                } else if (x == 9 && r < 0) {
+                    y = this.tablero[x].length;  // ?
+                    r = (int) (Math.random() * 9 + 0);
+                    this.tablero[r][y] = 2;
+                    this.posTesoro = new Posicion(r,y);
+
+                } else if (x != 0 && x != 9) {
+                    int rult = (int) (Math.random() * 9 + 0);
+                    if (rult <= 1) {
+                        this.tablero[x][y] = 1;
+                    } else if (rult >= 2) {
+                        this.tablero[x][y] = 0;
+                    }
                 }
             }
         }
-        }
+    }
     public int[][] getTablero () {
-        return tablero;
+        return this.tablero;
     }
 
     public Posicion getPosTesoro () {
-        return posTesoro;
+        return this.posTesoro;
     }
-
 }
